93e561214707f803888fcfb9af3160a7679048e3
Kotaro Kitafusa <puppet0pierrot@gmail.com>
2020-01-18 15:46:38 +0900

    creating ajax setting

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,3 +1,17 @@
 $(function(){
-  console.log('hi there');
+  $('#new_post').on('click', function(e){
+    // var new_post = $('#new_post');
+    e.preventDefault();
+    var url = $(this).attr("action");
+    var formData = new FormData(this);
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  })
 });
